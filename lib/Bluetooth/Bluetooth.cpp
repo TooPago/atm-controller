6349959802dfa64b5ba0267f1e5fc05da8c93105
lomelidev <miguel@lomeli.io>
2021-08-29 18:43:12 -0500

    ESP_LOGI is commented on the bluetooth module

--- lib/Bluetooth/Bluetooth.cpp
+++ lib/Bluetooth/Bluetooth.cpp
@@ -48,27 +48,27 @@ class MySecurity : public BLESecurityCallbacks
 {
     uint32_t onPassKeyRequest()
     {
-        ESP_LOGI(LOG_TAG, "PassKeyRequest");
+        //ESP_LOGI(LOG_TAG, "PassKeyRequest");
         return 123456;
     }
     void onPassKeyNotify(uint32_t pass_key)
     {
-        ESP_LOGI(LOG_TAG, "The passkey Notify number:%d", pass_key);
+        //ESP_LOGI(LOG_TAG, "The passkey Notify number:%d", pass_key);
     }
     bool onConfirmPIN(uint32_t pass_key)
     {
-        ESP_LOGI(LOG_TAG, "The passkey YES/NO number:%d", pass_key);
+        //ESP_LOGI(LOG_TAG, "The passkey YES/NO number:%d", pass_key);
         vTaskDelay(5000);
         return true;
     }
     bool onSecurityRequest()
     {
-        ESP_LOGI(LOG_TAG, "SecurityRequest");
+        //ESP_LOGI(LOG_TAG, "SecurityRequest");
         return true;
     }
     void onAuthenticationComplete(esp_ble_auth_cmpl_t cmpl)
     {
-        ESP_LOGI(LOG_TAG, "Starting BLE work!");
+        //ESP_LOGI(LOG_TAG, "Starting BLE work!");
     }
 };
 
